268d69abe09853b94a987881c2a19b266083297a
Filip Martiník <martinik.filip01@gmail.com>
2025-02-05 20:21:00 +0100

    disable ssr completely for detection

--- src/lib/components/LandmarkDetection.svelte
+++ src/lib/components/LandmarkDetection.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	// Disable ssr, few dependencies must be run in browser, and it is difficult to manage them in onMount()
+	export const ssr = false;
 	/**
 	 * Hand Gesture Recognition Component
 	 *
